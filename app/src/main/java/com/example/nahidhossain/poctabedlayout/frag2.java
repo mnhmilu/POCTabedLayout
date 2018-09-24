c5cf1c41168e92c4393767ad5ad1bf0702302f52
nahid.hossain <nahid.hossain@bkash.com>
2018-09-24 16:10:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nahidhossain/poctabedlayout/frag2.java
@@ -0,0 +1,104 @@
+package com.example.nahidhossain.poctabedlayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+
+ * to handle interaction events.
+ * Use the {@link frag2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class frag2 extends Fragment  implements View.OnClickListener {
+
+    Button btn;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public frag2() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment frag2.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static frag2 newInstance(String param1, String param2) {
+        frag2 fragment = new frag2();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void onClick(final View v){
+
+        /* example of fragment switching exmaple
+
+        Frag3 fragment3 = new Frag3();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_content, fragment3);
+        fragmentTransaction.commit();
+
+        */
+
+        /* example of fragment to activity switching */
+        Intent in = new Intent(getActivity(),Main2Activity.class);
+        in.putExtra("some","some data");
+        startActivity(in);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_frag2, container,         false);
+
+        btn = (Button) rootView.findViewById(R.id.button);
+        btn.setOnClickListener(this);
+
+        return  rootView;
+       // return inflater.inflate(R.layout.fragment_frag2, container, false);
+    }
+
+
+}
