352827b7bfd2409d28edc1b1a34669f1a2e6ee09
nahid.hossain <nahid.hossain@bkash.com>
2018-09-27 12:25:18 +0600

    Little Clean up

--- app/src/main/java/com/example/nahidhossain/poctabedlayout/Frag1.java
+++ app/src/main/java/com/example/nahidhossain/poctabedlayout/Frag1.java
@@ -1,6 +1,7 @@
 package com.example.nahidhossain.poctabedlayout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,7 +24,11 @@ import java.util.List;
  * Use the {@link Frag1#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Frag1 extends Fragment {
+public class Frag1 extends Fragment implements View.OnClickListener {
+
+    Button btn;
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,9 +38,6 @@ public class Frag1 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    String[] NAMES ={"Name A","Name B"};
-
-    String[] DESCRIPTIONS ={"Description 1", "Description 2"};
 
     public Frag1() {
         // Required empty public constructor
@@ -46,7 +49,7 @@ public class Frag1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Frag1.
+     * @return A new instance of fragment frag2.
      */
     // TODO: Rename and change types and number of parameters
     public static Frag1 newInstance(String param1, String param2) {
@@ -58,14 +61,32 @@ public class Frag1 extends Fragment {
         return fragment;
     }
 
+    public void onClick(final View v){
+
+        /* example of fragment switching exmaple
+
+        Frag3 fragment3 = new Frag3();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_content, fragment3);
+        fragmentTransaction.commit();
+
+        */
+
+        /* example of fragment to activity switching */
+        Intent in = new Intent(getActivity(),Main2Activity.class);
+        in.putExtra("some","some data");
+        startActivity(in);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
@@ -73,53 +94,13 @@ public class Frag1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View rootView =inflater.inflate(R.layout.fragment_frag1, container, false);
-        ListView listView=(ListView) rootView.findViewById(R.id.listView);
-        CustomeAdapter customeAdapter = new CustomeAdapter();
-        listView.setAdapter(customeAdapter);
+        View rootView = inflater.inflate(R.layout.fragment_frag1, container,         false);
 
-        return rootView;
+        btn = (Button) rootView.findViewById(R.id.button);
+        btn.setOnClickListener(this);
+
+        return  rootView;
+        // return inflater.inflate(R.layout.fragment_frag2, container, false);
     }
-
-    class  CustomeAdapter extends BaseAdapter{
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.customlayout,null);
-
-            TextView name = (TextView) convertView.findViewById(R.id.textView_name);
-            TextView description = (TextView) convertView.findViewById(R.id.textView_desc);
-
-            name.setText(NAMES[position]);
-            description.setText(DESCRIPTIONS[position]);
-
-            return  convertView;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/nahidhossain/poctabedlayout/frag2.java
+++ app/src/main/java/com/example/nahidhossain/poctabedlayout/frag2.java
@@ -1,16 +1,13 @@
 package com.example.nahidhossain.poctabedlayout;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
@@ -21,10 +18,11 @@ import android.widget.Button;
  * Use the {@link frag2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class frag2 extends Fragment  implements View.OnClickListener {
+public class frag2 extends Fragment   {
 
-    Button btn;
 
+
+///////////////////
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +33,9 @@ public class frag2 extends Fragment  implements View.OnClickListener {
     private String mParam1;
     private String mParam2;
 
+    String[] NAMES ={"Name A","Name B"};
+
+    String[] DESCRIPTIONS ={"Description 1", "Description 2"};
 
     public frag2() {
         // Required empty public constructor
@@ -46,7 +47,7 @@ public class frag2 extends Fragment  implements View.OnClickListener {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment frag2.
+     * @return A new instance of fragment Frag1.
      */
     // TODO: Rename and change types and number of parameters
     public static frag2 newInstance(String param1, String param2) {
@@ -58,32 +59,14 @@ public class frag2 extends Fragment  implements View.OnClickListener {
         return fragment;
     }
 
-    public void onClick(final View v){
-
-        /* example of fragment switching exmaple
-
-        Frag3 fragment3 = new Frag3();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.main_content, fragment3);
-        fragmentTransaction.commit();
-
-        */
-
-        /* example of fragment to activity switching */
-        Intent in = new Intent(getActivity(),Main2Activity.class);
-        in.putExtra("some","some data");
-        startActivity(in);
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -91,17 +74,43 @@ public class frag2 extends Fragment  implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View rootView = inflater.inflate(R.layout.fragment_frag2, container,         false);
+        View rootView =inflater.inflate(R.layout.fragment_frag2, container, false);
+        ListView listView=(ListView) rootView.findViewById(R.id.listView);
+        CustomeAdapter customeAdapter = new CustomeAdapter();
+        listView.setAdapter(customeAdapter);
 
-        btn = (Button) rootView.findViewById(R.id.button);
-        btn.setOnClickListener(this);
-
-        return  rootView;
-       // return inflater.inflate(R.layout.fragment_frag2, container, false);
+        return rootView;
     }
 
+    class  CustomeAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+            return 2;
+        }
 
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
 
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.customlayout,null);
+
+            TextView name = (TextView) convertView.findViewById(R.id.textView_name);
+            TextView description = (TextView) convertView.findViewById(R.id.textView_desc);
+
+            name.setText(NAMES[position]);
+            description.setText(DESCRIPTIONS[position]);
+
+            return  convertView;
+        }
+    }
 
 }
